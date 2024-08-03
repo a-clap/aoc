ab0374a9b95a5e631cbca6cc86963c89ccf3a893
a-clap <clapinskiadam@gmail.com>
2024-08-03 12:37:21 +0200

    feat(2015): day1

--- 2015/day1/day1.cpp
+++ 2015/day1/day1.cpp
@@ -1,4 +1,4 @@
-#include <format>
+#include <string_view>
 
 #include <gtest/gtest.h>
 
@@ -6,18 +6,36 @@
 
 #include "input.hpp"
 
-auto count_floors(std::string_view input) {
-    static constexpr auto kUp = '(';
-    static constexpr auto kDown = ')';
+static constexpr auto kUp = '(';
+[[maybe_unused]] static constexpr auto kDown = ')';
 
-    auto const up = std::ranges::count_if(input, [](auto ch) -> bool {
-        return ch == kUp;
-    });
-    auto const down = std::ranges::count_if(input, [](auto ch) -> bool {
-        return ch == kDown;
-    });
+static auto count_floors(std::string_view input) {
 
-    return up - down;
+    return std::ranges::fold_left(input.begin(), input.end(), 0, [](auto sum, auto sign) {
+        if (sign == kUp) {
+            return ++sum;
+        }
+        return --sum;
+    });
+}
+
+static auto count_floors_2(std::string_view input) {
+    static constexpr auto kDestination = -1;
+    auto start_floor = 0;
+
+    auto pos = 0;
+    for (auto const& character : input) {
+        ++pos;
+        if (character == kUp) {
+            ++start_floor;
+            continue;
+        }
+
+        if (--start_floor == kDestination) {
+            return pos;
+        }
+    }
+    return -1;
 }
 
 TEST(day1, input_1) {
@@ -27,7 +45,7 @@ TEST(day1, input_1) {
         int expected;
     };
 
-    args_t args[] = {
+    static constexpr args_t kArgs[] = {
         {
             .input = "(())",
             .expected = 0,
@@ -65,13 +83,16 @@ TEST(day1, input_1) {
             .expected = -3,
         },
     };
-    for (auto const& arg : args) {
-        SCOPED_TRACE(arg.input);
+    for (auto const& [input, expected] : kArgs) {
+        SCOPED_TRACE(input);
 
-        auto floors = count_floors(arg.input);
-        ASSERT_EQ(floors, arg.expected);
+        auto floors = count_floors(input);
+        ASSERT_EQ(floors, expected);
     }
 
     auto const answer = count_floors(kInput);
     ASSERT_EQ(answer, 74);
+
+    auto const answer2 = count_floors_2(kInput);
+    ASSERT_EQ(answer2, 1795);
 }
